6c25829af10f5a09bbf8c5c73bdfda172b414af6
Tamara Temple <tamouse@gmail.com>
2017-11-29 01:16:50 -0600

    utils for links

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,56 @@ import React from "react"
 import "./index.css"
 import styled from "styled-components"
 import Link from 'gatsby-link'
+import {linkLabel, linkPath} from '../utils'
+
+
+export default ({ data, children }) => {
+  console.log("from layout/index default", data.allFile.edges)
+  return (
+    <Page>
+      <Sidebar>
+        <SidebarTitle>Recipes</SidebarTitle>
+        <CategoriesNav>
+          {data.allFile.edges.map((item, index) => <CategoryItem key={index}>
+            <NavLink to={linkPath(item.node.relativePath)}>
+              {linkLabel(item.node.relativePath)}
+            </NavLink>
+          </CategoryItem>)}
+        </CategoriesNav>
+        <SidebarSiteNav>
+          <h3>Site Navigation</h3>
+          <ul>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/about">About</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+        </SidebarSiteNav>
+      </Sidebar>
+      <Content>
+        <ContentTitle>Contents</ContentTitle>
+        {children()}
+      </Content>
+    </Page>
+  )
+}
+
+export const query = graphql`
+  query NavRecipeCategories{
+    allFile(filter: {
+      relativePath: {
+        glob: "pages/recipes/**/index.js"
+      }
+    }) {
+      totalCount
+      edges {
+        node {
+          relativePath
+        }
+      }
+    }
+  }
+
+`
 
 const NavLink = styled(Link)`
   color: Papayawhip;
@@ -45,22 +95,10 @@ const ContentTitle = styled.h1`
   color: PaleVioletRed;
 `
 
-export default ({ children }) => (
-  <Page>
-    <Sidebar>
-      <SidebarTitle>Recipes</SidebarTitle>
-      <SidebarSiteNav>
-        <h3>Site Navigation</h3>
-        <ul>
-          <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/about">About</NavLink></li>
-          <li><NavLink to="/contact">Contact</NavLink></li>
-        </ul>
-      </SidebarSiteNav>
-    </Sidebar>
-    <Content>
-      <ContentTitle>Contents</ContentTitle>
-      {children()}
-    </Content>
-  </Page>
-)
+const CategoriesNav = styled.ul`
+
+`
+
+const CategoryItem = styled.li`
+  color: PapayaWhip;
+`
